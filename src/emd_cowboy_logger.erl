0478824c5ee9a2a3b8a7f75314d850b730abbdff
Anton Vilhelm Ásgeirsson <anton.v.asgeirsson@gmail.com>
2020-06-06 17:27:34 +0000

    Add a logging middleware

---
+++ src/emd_cowboy_logger.erl
@@ -0,0 +1,36 @@
+%%%-------------------------------------------------------------------
+%%% @author  Anton Vilhelm Ásgeirsson <anton.v.asgeirsson@gmail.com>
+%%% @copyright (C) 2020, Anton Vilhelm Ásgeirsson
+%%% @doc HTTP logging middleware
+%%% @end
+%%%-------------------------------------------------------------------
+
+%%%-------------------------------------------------------------------
+%% This file is part of ematrixd.
+%%
+%% ematrixd is free software: you can redistribute it and/or modify
+%% it under the terms of the GNU Affero General Public License as
+%% published by the Free Software Foundation, either version 3 of the
+%% License, or  (at your option) any later version.
+%%
+%% ematrixd is distributed in the hope that it will be useful,
+%% but WITHOUT ANY WARRANTY; without even the implied warranty of
+%% MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+%% GNU Affero General Public License for more details.
+%%
+%% You should have received a copy of the GNU Affero General Public
+%% License along with ematrixd.
+%% If not, see <https://www.gnu.org/licenses/>.
+%%%-------------------------------------------------------------------
+
+-module(emd_cowboy_logger).
+
+-behaviour(cowboy_middleware).
+
+-export([execute/2]).
+
+execute(Req, Env) ->
+    Method = cowboy_req:method(Req),
+    Path = cowboy_req:path(Req),
+    logger:log(info,"~s\t~s",[Method,Path]),
+    {ok, Req, Env}.
